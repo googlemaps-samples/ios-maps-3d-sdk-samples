49983e6fd631c8b516a6549f0a2ae0adc10801a7
Ed Boiling <edboiling@google.com>
2025-04-25 14:06:57 +0100

    Added gitignore

---
+++ GoogleMaps3DSampleApp/SampleApp/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleMaps3DSampleAppApp.swift
+//  GoogleMaps3DSampleApp
+//
+//  Created by Ed Boiling on 25/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct GoogleMaps3DSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
